87abe813ba374534103c23eb025e3714fc0cc27e
Yana T <yana.work.related@gmail.com>
2024-08-05 16:07:20 -0700

    added assignment entity

---
+++ src/main/java/com/hcc/entities/Assignment.java
@@ -0,0 +1,117 @@
+package com.hcc.entities;
+
+import java.util.Objects;
+
+public class Assignment {
+    private long id;
+    private String status;
+    private Integer number;
+    private String githubUrl;
+    private String branch;
+    private String reviewVideoUrl;
+    private User user;
+    private User codeReviewer;
+
+    public Assignment(long id, String status, Integer number, String githubUrl, String branch,
+                      String reviewVideoUrl, User user, User codeReviewer) {
+        this.id = id;
+        this.status = status;
+        this.number = number;
+        this.githubUrl = githubUrl;
+        this.branch = branch;
+        this.reviewVideoUrl = reviewVideoUrl;
+        this.user = user;
+        this.codeReviewer = codeReviewer;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getGithubUrl() {
+        return githubUrl;
+    }
+
+    public void setGithubUrl(String githubUrl) {
+        this.githubUrl = githubUrl;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public String getReviewVideoUrl() {
+        return reviewVideoUrl;
+    }
+
+    public void setReviewVideoUrl(String reviewVideoUrl) {
+        this.reviewVideoUrl = reviewVideoUrl;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getCodeReviewer() {
+        return codeReviewer;
+    }
+
+    public void setCodeReviewer(User codeReviewer) {
+        this.codeReviewer = codeReviewer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Assignment)) return false;
+        Assignment that = (Assignment) o;
+        return id == that.id && Objects.equals(status, that.status) && Objects.equals(number, that.number) && Objects.equals(githubUrl, that.githubUrl) && Objects.equals(branch, that.branch) && Objects.equals(reviewVideoUrl, that.reviewVideoUrl) && Objects.equals(user, that.user) && Objects.equals(codeReviewer, that.codeReviewer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, status, number, githubUrl, branch, reviewVideoUrl, user, codeReviewer);
+    }
+
+    @Override
+    public String toString() {
+        return "Assignment{" +
+                "id=" + id +
+                ", status='" + status + '\'' +
+                ", number=" + number +
+                ", githubUrl='" + githubUrl + '\'' +
+                ", branch='" + branch + '\'' +
+                ", reviewVideoUrl='" + reviewVideoUrl + '\'' +
+                ", user=" + user +
+                ", codeReviewer=" + codeReviewer +
+                '}';
+    }
+}
