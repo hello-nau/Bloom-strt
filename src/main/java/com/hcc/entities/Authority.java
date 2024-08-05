ef5e9c6957213a3ef7963201f13c27bd5e999fba
Yana T <yana.work.related@gmail.com>
2024-08-05 16:02:00 -0700

    added authority entity

---
+++ src/main/java/com/hcc/entities/Authority.java
@@ -0,0 +1,61 @@
+package com.hcc.entities;
+
+import java.util.Objects;
+
+public class Authority {
+    private long id;
+    private String authority;
+    private User user;
+
+    public Authority(long id, String authority, User user) {
+        this.id = id;
+        this.authority = authority;
+        this.user = user;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Authority)) return false;
+        Authority authority1 = (Authority) o;
+        return id == authority1.id && Objects.equals(authority, authority1.authority) && Objects.equals(user, authority1.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, authority, user);
+    }
+
+    @Override
+    public String toString() {
+        return "Authority{" +
+                "id=" + id +
+                ", authority='" + authority + '\'' +
+                ", user=" + user +
+                '}';
+    }
+}
