e08ba05d50b61624e2205b684d8cfef1f0f26b25
Yana T <yana.work.related@gmail.com>
2024-08-05 17:50:39 -0700

    uncommented userDetailService and customPassword encoder

--- src/main/java/com/hcc/services/UserDetailServiceImpl.java
+++ src/main/java/com/hcc/services/UserDetailServiceImpl.java
@@ -1,10 +1,11 @@
 package com.hcc.services;
 
-// import com.hcc.entities.User;
+ import com.hcc.entities.User;
 // import com.hcc.repositories.UserRepository;
 // import com.hcc.utils.CustomPasswordEncoder;
 // import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.security.core.userdetails.UserDetails;
+ import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.security.core.userdetails.UserDetailsService;
 // import org.springframework.security.core.userdetails.UsernameNotFoundException;
 //import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,11 +23,33 @@ import java.util.Optional;
 //    @Autowired
     // UserRepository userRepo;
 
- //   @Override
+//    @Override
 //    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-      //  Optional<User> userOpt = userRepo.findByUsername(username);
+//        Optional<User> userOpt = userRepo.findByUsername(username);
 //        user.setUsername(username);
 //        user.setPassword(passwordEncoder.getPasswordEncoder().encode("asdfasdf"));
-      //  return userOpt.orElseThrow(() -> new UsernameNotFoundException("Invalid Credentials"));
+//        return userOpt.orElseThrow(() -> new UsernameNotFoundException("Invalid Credentials"));
 //    }
 //}
+
+ import com.hcc.entities.User;
+ import com.hcc.utils.CustomPasswordEncoder;
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.security.core.userdetails.UserDetails;
+ import org.springframework.security.core.userdetails.UserDetailsService;
+ import org.springframework.security.core.userdetails.UsernameNotFoundException;
+ import org.springframework.security.crypto.password.PasswordEncoder;
+ import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+    @Autowired
+    CustomPasswordEncoder passwordEncoder;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.getPasswordEncoder().encode("asdfasdf"));
+        return user;
+    }
+}
