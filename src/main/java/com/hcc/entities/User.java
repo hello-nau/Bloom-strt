f1103fb1b413cea9062cdc98751e3bc45feaf9bc
Yana T <yana.work.related@gmail.com>
2024-08-05 18:23:56 -0700

    modified user to implement UserDetails spring security

--- src/main/java/com/hcc/entities/User.java
+++ src/main/java/com/hcc/entities/User.java
@@ -1,11 +1,12 @@
 package com.hcc.entities;
 
-import java.time.LocalDate;
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-public class User {
+import java.time.LocalDate;
+import java.util.*;
+
+public class User implements UserDetails {
    private long id;
     private LocalDate cohortStartDate;
     private String username;
@@ -22,45 +23,65 @@ public class User {
     public User() {
     }
 
-    public String getPassword() {
-        return password;
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//    public long getId() {
+//        return id;
+//    }
+//
+//    public void setId(long id) {
+//        this.id = id;
+//    }
+//
+//    public LocalDate getCohortStartDate() {
+//        return cohortStartDate;
+//    }
+//
+//    public void setCohortStartDate(LocalDate cohortStartDate) {
+//        this.cohortStartDate = cohortStartDate;
+//    }
+@Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new Authority("role_student"));
+        return roles;
     }
+@Override
+public String getPassword() {
+    return password;
+}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public LocalDate getCohortStartDate() {
-        return cohortStartDate;
-    }
-
-    public void setCohortStartDate(LocalDate cohortStartDate) {
-        this.cohortStartDate = cohortStartDate;
-    }
-
+    @Override
     public String getUsername() {
         return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public void setUsername(String username) {
         this.username = username;
     }
 
-    public List<Authority> getAuthorities() {
-        return authorities;
-    }
 
-    public void setAuthorities(List<Authority> authorities) {
-        this.authorities = authorities;
-    }
 
     @Override
     public boolean equals(Object o) {
