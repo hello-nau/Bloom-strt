ad22b6fa835efc0df0674d26cd2d3d8f737eef42
Yana T <yana.work.related@gmail.com>
2024-08-05 18:37:50 -0700

    filled enum classes

--- src/main/java/com/hcc/enums/AuthorityEnum.java
+++ src/main/java/com/hcc/enums/AuthorityEnum.java
@@ -1,4 +1,9 @@
 package com.hcc.enums;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum AuthorityEnum {
+    ROLE_LEARNER,
+    ROLE_REVIEWER,
+    ROLE_ADMIN;
 }
