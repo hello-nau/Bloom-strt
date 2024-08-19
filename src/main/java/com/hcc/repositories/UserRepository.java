c96c3a0622c449339e6961b1152ce8313413249b
Yana T <yana.work.related@gmail.com>
2024-08-19 15:51:31 -0700

    updated userRepository interface

--- src/main/java/com/hcc/repositories/UserRepository.java
+++ src/main/java/com/hcc/repositories/UserRepository.java
@@ -1,10 +1,14 @@
 package com.hcc.repositories;
 
+import com.hcc.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 
-public class UserRepository {
-    public Optional<UserDetails> findByUsername(String usernameFromToken) {
-    }
+    Optional<User> findByUsername(String username);
 }
