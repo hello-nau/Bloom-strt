47e0f34833ea871e9eb8156d23ff2f725a8f9827
Yana T <yana.work.related@gmail.com>
2024-08-22 14:12:36 -0700

    fixed custompasswordencoder

--- src/main/java/com/hcc/config/SecurityConfig.java
+++ src/main/java/com/hcc/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.http.HttpServletResponse;
@@ -28,6 +30,7 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
--- src/main/java/com/hcc/services/AuthServiceImpl.java
+++ src/main/java/com/hcc/services/AuthServiceImpl.java
@@ -2,6 +2,7 @@ package com.hcc.services;
 
 import com.hcc.entities.User;
 import com.hcc.repositories.UserRepository;
+import com.hcc.utils.CustomPasswordEncoder;
 import com.hcc.utils.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,13 +15,13 @@ public class AuthServiceImpl implements AuthService{
     @Autowired
     UserRepository userRepository;
     @Autowired
-    PasswordEncoder passwordEncoder;
+    CustomPasswordEncoder passwordEncoder;
     @Autowired
     JWTUtil jwtUtil;
     @Override
     public Optional<String> authenticate(String username, String password) {
         Optional<User> user = userRepository.findByUsername(username);
-        if(user.isEmpty() || !passwordEncoder.matches(password, user.get().getPassword()))
+        if(user.isEmpty() || !passwordEncoder.getPasswordEncoder().matches(password, user.get().getPassword()))
             return Optional.empty();
 
         return Optional.of(jwtUtil.generateToken(user.get()));
