80eaf4882127ec6d26efa157103de602eb41bd87
Yana T <yana.work.related@gmail.com>
2024-08-21 18:35:06 -0700

    implemented authController api

--- src/main/java/com/hcc/controllers/AuthController.java
+++ src/main/java/com/hcc/controllers/AuthController.java
@@ -2,13 +2,17 @@ package com.hcc.controllers;
 
 
 import com.hcc.controllers.requests.AuthCredentialsRequest;
+import com.hcc.controllers.responses.LoginResponse;
 import com.hcc.services.AuthService;
 import com.hcc.utils.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
+import java.util.Map;
 import java.util.Optional;
 
 // this is an example controller feel free to delete this once you have created your own.
@@ -23,21 +27,25 @@ public class AuthController {
     UserDetails userDetails;
 
     @PostMapping("/login")
-    public ResponseEntity<?> generateUserToken(@RequestBody AuthCredentialsRequest request) {
+    public ResponseEntity<LoginResponse> login(@RequestBody AuthCredentialsRequest request) {
         Optional<String> token = authService.authenticate(request.getUsername(), request.getPassword());
+        if (token.isEmpty()) return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
 
-        return ResponseEntity.ok(new JWTUtil());
+        return ResponseEntity.ok(new LoginResponse(token.get()));
     }
+
     @PostMapping("/validate")
-    public ResponseEntity<?> validateToken() {
-        jwtUtil.validateToken(token, userDetails);
+    public ResponseEntity<Map<String, Boolean>> validateToken(@RequestHeader("Autorization")
+                                                              String authorizationHeader) {
+        if (authorizationHeader == null || authorizationHeader.startsWith("Bearer ")) {
+            return ResponseEntity.badRequest().build();
+        }
+        String token = authorizationHeader.substring(7);
+        return ResponseEntity.ok(Collections.singletonMap("isValid", isTokenValid(token)));
     }
 
-
-
-//    @GetMapping
-//    ResponseEntity<?> welcomeLearner() {
-//        Hello greeting = helloService.greetLearner();
-//        return ResponseEntity.ok(greeting);
-//    }
+    private boolean isTokenValid(String token) {
+        return jwtUtil.validateToken(token, userDetails);
+    }
 }
+
--- src/main/java/com/hcc/controllers/responses/LoginResponse.java
+++ src/main/java/com/hcc/controllers/responses/LoginResponse.java
@@ -1,5 +1,16 @@
 package com.hcc.controllers.responses;
 
 public class LoginResponse {
+    private String token;
+    private boolean isValid;
+
+    public LoginResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
 
 }
