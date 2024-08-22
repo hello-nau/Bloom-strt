dec829be48071bfc562108f1c33c8db4e890d176
Yana T <yana.work.related@gmail.com>
2024-08-21 18:47:00 -0700

    changed some minor details

--- src/main/java/com/hcc/controllers/responses/LoginResponse.java
+++ src/main/java/com/hcc/controllers/responses/LoginResponse.java
@@ -2,7 +2,6 @@ package com.hcc.controllers.responses;
 
 public class LoginResponse {
     private String token;
-    private boolean isValid;
 
     public LoginResponse(String token) {
         this.token = token;
--- src/main/java/com/hcc/repositories/UserRepository.java
+++ src/main/java/com/hcc/repositories/UserRepository.java
@@ -3,7 +3,6 @@ package com.hcc.repositories;
 import com.hcc.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
